d8006707235e940557bdc9d045788737d884585d
TomasRacciatti <tomasracciatti@gmail.com>
2025-11-27 20:03:57 -0300

    Item Amount script
    Handles adding, removing, and clamping items

---
+++ TalesOfOsorios/Assets/Scripts/Items/Core/ItemAmount.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using System;
+
+namespace Items.Core
+{
+    [Serializable]
+    public class ItemAmount
+    {
+        [SerializeField] private SoItem soItem;
+        [SerializeField] private int amount;
+        
+        public SoItem SoItem => soItem;
+        public int Amount => amount;
+        public int Stack => soItem != null ? soItem.Stack : 0;
+        public bool ValidSoItem => soItem != null;
+        public bool IsEmpty => soItem == null || amount <= 0;
+        public bool IsFull => soItem != null && amount >= Stack;
+        
+
+        public ItemAmount(ItemAmount newItemAmount)
+        {
+            soItem = newItemAmount.SoItem;
+            amount = newItemAmount.Amount;
+        }
+
+        public ItemAmount(SoItem newSoItem, int newAmount)
+        {
+            soItem = newSoItem;
+            amount = newAmount;
+        }
+        
+        public int SetItem(ItemAmount itemAmount)
+        {
+            if (itemAmount == null || itemAmount.IsEmpty)
+            {
+                Clear();
+                return 0;
+            }
+
+            soItem = itemAmount.SoItem;
+            return SetAmount(itemAmount.Amount); 
+        }
+
+        public void SetItem(SoItem newItem)
+        {
+            soItem = newItem;
+        }
+        
+        public int SetAmount(int newAmount)
+        {
+            if (!ValidSoItem)
+                return newAmount;
+
+            int clampedAmount = Mathf.Clamp(newAmount, 0, Stack);
+
+            amount = clampedAmount;
+
+            if (amount <= 0)
+                Clear();
+
+            return newAmount - clampedAmount; 
+        }
+        
+        public int AddAmount(int amountToAdd)
+        {
+            if (IsEmpty || amountToAdd <= 0) return amountToAdd;
+            return SetAmount(amount + amountToAdd);
+        }
+        
+        public int RemoveAmount(int amountToRemove)
+        {
+            if (IsEmpty || amountToRemove <= 0) return amountToRemove;
+            return SetAmount(amount - amountToRemove);
+        }
+
+        public void Clear()
+        {
+            soItem = null;
+            amount = 0;
+        }
+    }
+}
