fcd54a24bad4e9b91628e9f6662692ca00b12c57
TomasRacciatti <tomasracciatti@gmail.com>
2025-11-28 12:36:27 -0300

    missing namespaces

--- TalesOfOsorios/Assets/Scripts/Items/Inventory/InvSlotUI.cs
+++ TalesOfOsorios/Assets/Scripts/Items/Inventory/InvSlotUI.cs
@@ -1,55 +1,57 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 using Items.Core;
-using Items.Inventory;
 using Managers;
 
-public class InvSlotUI : MonoBehaviour, IDropHandler
+namespace Items.Inventory
 {
-    private InvView invView;
-    private int invSlot;
-        
-    public InvView InvView => invView;
-    public int InvSlot => invSlot;
-        
-    public void Initialize(InvView inventoryView, int inventorySlot)
+    public class InvSlotUI : MonoBehaviour, IDropHandler
     {
-        invView = inventoryView;
-        invSlot = inventorySlot;
-    }
-        
-    public void SetItem(ItemAmount itemAmount)
-    {
-        InvItemUI itemUI = GetComponentInChildren<InvItemUI>();
-            
-        if (itemAmount == null || itemAmount.IsEmpty)
+        private InvView invView;
+        private int invSlot;
+
+        public InvView InvView => invView;
+        public int InvSlot => invSlot;
+
+        public void Initialize(InvView inventoryView, int inventorySlot)
         {
-            if (itemUI != null)
+            invView = inventoryView;
+            invSlot = inventorySlot;
+        }
+
+        public void SetItem(ItemAmount itemAmount)
+        {
+            InvItemUI itemUI = GetComponentInChildren<InvItemUI>();
+
+            if (itemAmount == null || itemAmount.IsEmpty)
             {
-                Destroy(itemUI.gameObject);
+                if (itemUI != null)
+                {
+                    Destroy(itemUI.gameObject);
+                }
+
+                return;
             }
-            return;
+
+            if (itemUI == null)
+            {
+                GameObject newItem = Instantiate(PrefabsManager.ItemPrefabUI, Vector3.zero, Quaternion.identity);
+                itemUI = newItem.GetComponent<InvItemUI>();
+                itemUI.SetSlotUI(this);
+
+                newItem.transform.SetParent(transform, false);
+                newItem.transform.localPosition = Vector3.zero;
+                newItem.transform.localRotation = Quaternion.identity;
+            }
+
+            itemUI.ShowItem(itemAmount);
         }
 
-        if (itemUI == null)
+        public void OnDrop(PointerEventData eventData)
         {
-            GameObject newItem = Instantiate(PrefabsManager.ItemPrefabUI, Vector3.zero, Quaternion.identity);
-            itemUI = newItem.GetComponent<InvItemUI>();
-            itemUI.SetSlotUI(this);
-                
-            newItem.transform.SetParent(transform, false);
-            newItem.transform.localPosition = Vector3.zero;
-            newItem.transform.localRotation = Quaternion.identity;
-        }
-
-        itemUI.ShowItem(itemAmount);
-    }
-        
-    public void OnDrop(PointerEventData eventData)
-    {
-       InvItemUI fromItemUI = eventData.pointerDrag?.GetComponent<InvItemUI>();
+            InvItemUI fromItemUI = eventData.pointerDrag?.GetComponent<InvItemUI>();
             if (fromItemUI == null) return;
-            
+
             InvSlotUI fromSlotUI = fromItemUI.SlotUI;
             if (fromSlotUI == null) return;
 
@@ -57,5 +59,6 @@ public class InvSlotUI : MonoBehaviour, IDropHandler
 
             fromSlotUI.InvView.InventorySystem.TransferIndexToIndex(
                 InvView.InventorySystem, fromSlotUI.InvSlot, InvSlot);
+        }
     }
 }
--- TalesOfOsorios/Assets/Scripts/Items/Inventory/InvView.cs
+++ TalesOfOsorios/Assets/Scripts/Items/Inventory/InvView.cs
@@ -1,54 +1,57 @@
-using UnityEngine;
 using Items.Core;
+using UnityEngine;
 
-public class InvView : MonoBehaviour
+namespace Items.Inventory
 {
-    [SerializeField] private InvSlotUI[] slots;
-    [SerializeField] private InvSystem inventorySystem;
-        
-    public InvSystem InventorySystem => inventorySystem;
-
-    private void Awake()
+    public class InvView : MonoBehaviour
     {
-        for (int i = 0; i < slots.Length; i++)
+        [SerializeField] private InvSlotUI[] slots;
+        [SerializeField] private InvSystem inventorySystem;
+        
+        public InvSystem InventorySystem => inventorySystem;
+
+        private void Awake()
         {
-            slots[i].Initialize(this, i);
+            for (int i = 0; i < slots.Length; i++)
+            {
+                slots[i].Initialize(this, i);
+            }
         }
-    }
 
-    private void Start()
-    {
-        if (inventorySystem != null)
-            SetInventory(inventorySystem);
-    }
-        
-    public void SetInventory(InvSystem newInventory)
-    {
-        if (inventorySystem != null)
-            inventorySystem.OnItemChanged -= OnItemChanged;
-
-        inventorySystem = newInventory;
-
-        if (inventorySystem == null) return;
-
-        inventorySystem.OnItemChanged += OnItemChanged;
-        UpdateAllSlots();
-    }
-
-    private void OnItemChanged(int index, ItemAmount item)
-    {
-        if (index < 0 || index >= slots.Length) return;
-        slots[index].SetItem(item);
-    }
-
-    private void UpdateAllSlots()
-    {
-        var items = inventorySystem.Items;
-
-        for (int i = 0; i < slots.Length; i++)
+        private void Start()
         {
-            if (i < items.Count)
-                slots[i].SetItem(items[i]);
+            if (inventorySystem != null)
+                SetInventory(inventorySystem);
+        }
+        
+        public void SetInventory(InvSystem newInventory)
+        {
+            if (inventorySystem != null)
+                inventorySystem.OnItemChanged -= OnItemChanged;
+
+            inventorySystem = newInventory;
+
+            if (inventorySystem == null) return;
+
+            inventorySystem.OnItemChanged += OnItemChanged;
+            UpdateAllSlots();
+        }
+
+        private void OnItemChanged(int index, ItemAmount item)
+        {
+            if (index < 0 || index >= slots.Length) return;
+            slots[index].SetItem(item);
+        }
+
+        private void UpdateAllSlots()
+        {
+            var items = inventorySystem.Items;
+
+            for (int i = 0; i < slots.Length; i++)
+            {
+                if (i < items.Count)
+                    slots[i].SetItem(items[i]);
+            }
         }
     }
 }
