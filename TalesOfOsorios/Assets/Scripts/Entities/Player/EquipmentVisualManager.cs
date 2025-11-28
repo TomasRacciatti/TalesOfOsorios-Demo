00ea1020664fdc002ed11f4f128d5707deb98d36
TomasRacciatti <tomasracciatti@gmail.com>
2025-11-28 16:30:21 -0300

    created equipment visual manager
    this component will handle activating the sprites accordingly when equiping something

---
+++ TalesOfOsorios/Assets/Scripts/Entities/Player/EquipmentVisualManager.cs
@@ -0,0 +1,113 @@
+using System.Collections.Generic;
+using Items.Inventory;
+using UnityEngine;
+using Items.Core;
+
+namespace Entities.Player
+{
+    public class EquipmentVisualManager : MonoBehaviour
+    {
+        [Header("Equipment Visuals Mapping")]
+        [SerializeField] private List<EquipmentSlotVisuals> equipmentSlots = new List<EquipmentSlotVisuals>();
+
+        private Dictionary<EquipSlotId, List<GameObject>> slotVisualsMap;
+        
+        private void Awake()
+        {
+            InitializeSlotVisualsMap();
+            DisableAllEquipmentVisuals();
+        }
+        
+        public void Initialize(InvSystem equipInventory)
+        {
+            if (equipInventory == null)
+            {
+                return;
+            }
+
+            equipInventory.OnItemChanged += OnEquipmentChanged;
+
+            for (int i = 0; i < equipInventory.Items.Count; i++)
+            {
+                OnEquipmentChanged(i, equipInventory.Items[i]);
+            }
+        }
+        
+        private void InitializeSlotVisualsMap()
+        {
+            slotVisualsMap = new Dictionary<EquipSlotId, List<GameObject>>();
+
+            foreach (var slotVisual in equipmentSlots)
+            {
+                if (!slotVisualsMap.ContainsKey(slotVisual.slotId))
+                {
+                    slotVisualsMap[slotVisual.slotId] = new List<GameObject>();
+                }
+
+                slotVisualsMap[slotVisual.slotId].AddRange(slotVisual.visualObjects);
+            }
+        }
+
+        private void OnEquipmentChanged(int slotIndex, ItemAmount itemAmount)
+        {
+            if (itemAmount == null || itemAmount.IsEmpty)
+            {
+                return;
+            }
+
+            EquipSlotId slotId = itemAmount.SoItem.EquipSlotId;
+
+            if (slotId == EquipSlotId.None || slotId == EquipSlotId.Potion)
+            {
+                return;
+            }
+
+            UpdateSlotVisuals(slotId, !itemAmount.IsEmpty);
+        }
+        
+        private void UpdateSlotVisuals(EquipSlotId slotId, bool isEquipped)
+        {
+            if (!slotVisualsMap.ContainsKey(slotId))
+            {
+                Debug.LogWarning($"No visual mapping found for slot: {slotId}");
+                return;
+            }
+
+            foreach (var visualObject in slotVisualsMap[slotId])
+            {
+                if (visualObject != null)
+                {
+                    visualObject.SetActive(isEquipped);
+                }
+            }
+        }
+
+        private void DisableAllEquipmentVisuals()
+        {
+            foreach (var slotVisuals in slotVisualsMap.Values)
+            {
+                foreach (var visualObject in slotVisuals)
+                {
+                    if (visualObject != null)
+                    {
+                        visualObject.SetActive(false);
+                    }
+                }
+            }
+        }
+        
+        public void UnequipSlot(EquipSlotId slotId)
+        {
+            UpdateSlotVisuals(slotId, false);
+        }
+        
+        
+        [System.Serializable]
+        public class EquipmentSlotVisuals
+        {
+            public EquipSlotId slotId;
+            public List<GameObject> visualObjects = new List<GameObject>();
+        }
+    }
+
+}
