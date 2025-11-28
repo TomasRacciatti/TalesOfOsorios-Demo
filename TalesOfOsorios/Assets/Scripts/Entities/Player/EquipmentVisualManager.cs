bdbee9797de6a22332573475e71c590abd804dbf
TomasRacciatti <tomasracciatti@gmail.com>
2025-11-28 20:00:31 -0300

    removed corutine from EquipmentVisualMgr
    With the execution order solved, the corutine which helped before is no longer needed

--- TalesOfOsorios/Assets/Scripts/Entities/Player/EquipmentVisualManager.cs
+++ TalesOfOsorios/Assets/Scripts/Entities/Player/EquipmentVisualManager.cs
@@ -9,12 +9,12 @@ namespace Entities.Player
 {
     public class EquipmentVisualManager : MonoBehaviour
     {
-        [SerializeField, Tooltip("0: Legs, 1: Waist, 2: Chest, 3; Shoulder, 4: Arms, 5: Hand")] 
+        [SerializeField, Tooltip("0: Legs, 1: Waist, 2: Chest, 3; Shoulder, 4: Arms, 5: Hand")]
         private List<EquipmentSlot> equipmentVisuals;
-        
+
         private InvSystem _invSystem;
         private List<SpriteRenderer[]> _spriteRenderers;
-        
+
         private void Awake()
         {
             _spriteRenderers = new List<SpriteRenderer[]>();
@@ -22,9 +22,9 @@ namespace Entities.Player
             foreach (var slot in equipmentVisuals)
             {
                 if (slot == null || slot.visualObjects == null) continue;
-                
+
                 SpriteRenderer[] renderers = new SpriteRenderer[slot.visualObjects.Length];
-                
+
                 for (int i = 0; i < slot.visualObjects.Length; i++)
                 {
                     if (slot.visualObjects[i] != null)
@@ -32,21 +32,21 @@ namespace Entities.Player
                         renderers[i] = slot.visualObjects[i].GetComponent<SpriteRenderer>();
                     }
                 }
-                
+
                 _spriteRenderers.Add(renderers);
             }
         }
 
         private void Start()
         {
-            StartCoroutine(InitializeEquipmentSystem());
+            InitializeEquipmentSystem();
         }
 
 
         private void SetSprite(int index, ItemAmount itemAmount)
         {
             if (index < 0 || index >= equipmentVisuals.Count) return;
-            
+
             var renderers = _spriteRenderers[index];
             var visualObjects = equipmentVisuals[index].visualObjects;
 
@@ -56,13 +56,14 @@ namespace Entities.Player
                 {
                     if (go != null) go.SetActive(false);
                 }
+
                 return;
             }
 
             for (int i = 0; i < visualObjects.Length; i++)
             {
                 visualObjects[i].SetActive(true);
-            
+
                 if (i == 0 && itemAmount.SoItem.Icon != null)
                 {
                     renderers[i].sprite = itemAmount.SoItem.Icon;
@@ -71,29 +72,23 @@ namespace Entities.Player
                 {
                     renderers[i].sprite = itemAmount.SoItem.IconSecond;
                 }
-                
             }
         }
-        
-        private IEnumerator InitializeEquipmentSystem()
+
+        private void InitializeEquipmentSystem()
         {
-            while (_invSystem == null)
+            if (Managers.GameManager.Canvas != null &&
+                Managers.GameManager.Canvas.InvManager != null)
             {
-                if (Managers.GameManager.Canvas != null && 
-                    Managers.GameManager.Canvas.InvManager != null)
+                _invSystem = Managers.GameManager.Canvas.InvManager.EquipInventory;
+
+                if (_invSystem != null)
                 {
-                    _invSystem = Managers.GameManager.Canvas.InvManager.EquipInventory;
-                    
-                    if (_invSystem != null)
-                    {
-                        _invSystem.OnItemChanged += SetSprite;
-                        yield break;
-                    }
+                    _invSystem.OnItemChanged += SetSprite;
                 }
-                yield return null;
             }
         }
-        
+
         [System.Serializable]
         public class EquipmentSlot
         {
