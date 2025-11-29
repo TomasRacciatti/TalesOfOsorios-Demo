740729b791c79a338b08c740c759d4e6bcb6fe98
TomasRacciatti <tomasracciatti@gmail.com>
2025-11-28 22:46:46 -0300

    Inventory save data

---
+++ TalesOfOsorios/Assets/Scripts/SaveSystem/InventorySaveData.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+namespace SaveSystem
+{
+    public class InventorySaveData
+    {
+        public List<SerializableItemData> items;
+        
+        public InventorySaveData()
+        {
+            items = new List<SerializableItemData>();
+        }
+
+        public InventorySaveData(int size)
+        {
+            items = new List<SerializableItemData>(size);
+            for (int i = 0; i < size; i++)
+            {
+                items.Add(new SerializableItemData());
+            }
+        }
+    }
+}
